3f14886719602443aa9d167d9db618a07bde957d
JackConfetti <giacomoconfetti@gmail.com>
2017-03-16 20:00:41 +0100

    VERIFICA

---
+++ Verificozza/Componente gang.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verificozza
+{
+    class Componente_gang : Gang
+    {
+        public int BravuraFlow { get; set; }
+
+        //COSTRUTTORE DELLA CLASSE FIGLIA DI GANG Gli argomenti dichiarati nella classe figlia che sono in comunella con quelli della classe madre devono per forza avere o stesso nome
+
+        public Componente_gang(string Nomegang, string Mottogang, string AbbreviazioneGang, int numcomponentigang, int BravuraFlow) : base(Nomegang, Mottogang, AbbreviazioneGang, 0) // Lo 0 per il numero componenti
+        {
+            this.BravuraFlow = BravuraFlow;
+
+        }
+
+        public override string Rimadellagang()  //METODO EREDITATO so benissimo che è inutile e anzi sensa senso ed è per questo che non worka un cazzo di niente
+
+        {
+            return base.Rimadellagang();
+
+            string output = " vedo il mondo davvero";
+
+            Console.WriteLine(output);
+
+        }
+    }
+}
---
+++ Verificozza/Gang.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verificozza
+{
+    class Gang
+    {
+       //Le variabili private non devono esserci per forze , servono solo per immagazzinare il dato della propretà in maniera privata
+        public string Nomegang { get; set; }     // Proprietà in lettura e scrittura
+
+        private string _Mottogang;
+        public string Mottogang { get { return _Mottogang; } } // proprietà in sola lettura 
+
+        private string _Abbreviazionegang; // proprietà con filtro neutro
+        public string Abbreviazionegang
+        {
+            get
+            {
+                return _Abbreviazionegang; 
+            }
+
+            set
+            {
+                _Abbreviazionegang = value;
+            }
+        }
+
+        private int  _numcomponentigang;
+        public int  numcomponentigang { get { return _numcomponentigang; } }
+
+        public Gang (string Nomegang, string Mottogang, string Abbreviazonegang, int numcomponentigang)   // COSTRUTTORE 
+        {
+            _Mottogang = Mottogang;
+
+            _Abbreviazionegang = Abbreviazionegang;
+
+            if (numcomponentigang < 2) numcomponentigang = 777;                                             //Filtro di una proprietà in sola Lettura
+            _numcomponentigang = numcomponentigang;
+            
+
+
+        }
+
+        public override string ToString()                                   // OVERRIDE DEL TOSTRING Come è rappresentata quella istanza
+        {
+           return  Nomegang + ' ' + Abbreviazionegang;
+        }
+
+        public string describe()
+        {
+            string output = "Questa è la gang: "+ Nomegang +"\r\n";
+            output += "non provare a infamare la gang di " + numcomponentigang + " pischelli" + "\r\n";
+            return output;
+        }
+           
+        public virtual string Rimadellagang()
+        {
+            string output = "Perchè quando plano dall'alto ";
+            return output;
+
+            
+        }
+    }
+}
